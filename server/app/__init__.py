1ddc7946d9a7bc4ca05854d6e1c2b87d31462cce
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-12-01 10:19:01 +0300

    blueprints added in app

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -23,4 +23,24 @@ def create_app():
     ma.init_app(app)
     api.init_app(app)
 
+    from .api.user_api import user_bp
+    from .api.category_api import category_bp
+    from .api.order_api import order_bp
+    from .api.address_api import address_bp
+    from .api.product_api import product_bp
+    from .api.order_detail_api import order_detail_bp
+    from .api.auth_api import auth_bp
+    from .api.image_api import image_bp
+    from .api.chat_bot_api import chat_bot_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(address_bp)
+    app.register_blueprint(category_bp)
+    app.register_blueprint(order_bp)
+    app.register_blueprint(product_bp)
+    app.register_blueprint(order_detail_bp)
+    app.register_blueprint(image_bp)
+    app.register_blueprint(chat_bot_bp)
+
     return app
