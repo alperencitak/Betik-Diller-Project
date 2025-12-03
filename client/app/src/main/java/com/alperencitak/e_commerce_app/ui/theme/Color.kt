266a2411fa27babd8fcb1bad19bff26678c81092
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-12-03 13:15:22 +0300

    theme has been set

---
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.alperencitak.e_commerce_app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val SoftBeige = Color(0xFFB0ACA6)
+val DarkerSoftBeige = Color(0xFF9A958F)
+val LightGray = Color(0xFFC8C6C3)
+val Gray = Color(0xFF969695)
+val LightCream = Color(0xFFE2E3E2)
+val WhiteModern = Color(0xFFF5F5F5)
+val DarkGreen = Color(0xFF4BAE4F)
+val LightBlue = Color(0xFFBBDEFB)
+val Blue = Color(0xFF1975D0)
+val SmoothRed = Color(0xFFE57373)
+val DarkPurple = Color(0xFF6C3FE9)
+val Purple = Color(0xFF7176F4)
+val SmoothPurple = Color(0xFF7BAAFB)
+val LightPurple = Color(0x727176F4)
+val White = Color(0xFFE5EBE7)
